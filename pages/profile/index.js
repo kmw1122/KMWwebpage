f9f585d0f4a075e8d1adf94cabbad5cd12b2f56d
kmw1122 <112841874+kmw1122@users.noreply.github.com>
2022-12-07 22:55:28 +0900

    1-webpage

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -3,14 +3,20 @@ import Link from 'next/link'
 export default function ProfileDefault({ children }) {
   return (
     <div>
-      <h1>나의 포트폴리오 </h1>
+      <h1>나의 포트폴리오</h1>
+
       <div className="box1">
+        {' '}
+        <div>1학기</div>
+        <hr />
         1-1: Nextjs 테스트 예제 주소(github) : https://github.com/kmw1122/was
         1-2: Nextjs 테스트 예제 주소(vercel) : https://was-kmw1122.vercel.app/
         2-1: NextShop 예제 주소(github) : https://github.com/kmw1122/nextshop
         2-2: NextShop 예제 주소(vercel) : https://nextshop-z4ws.vercel.app/
       </div>
       <div className="box2">
+        <div>2학기</div>
+        <hr />
         1-1: Nextshop 테스트 예제 주소(github) :
         https://github.com/kmw1122/nextshop 1-2: Nextshop 테스트 예제
         주소(vercel) : 2-1: 개인 포트폴리오 웹사이트(github) :
