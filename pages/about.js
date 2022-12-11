9c589c41ce3803be58534bc964a64a6b7a9d39df
김민우 <kmw011122@gmail.com>
2022-12-11 20:32:09 +0900

    3

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,15 @@
+import react from 'react'
+import Image from 'next/image'
+
 export default function About() {
   return (
-    <div>
+    <div className="">
       <h1>개인정보</h1>
       <p>정보보호학과 3학년 김민우</p>
+
+      <img src="\images\image1.png" width="700" height="700"></img>
+
+      <img src="\images\image2.png" width="700" height="700"></img>
     </div>
   )
 }
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -4,6 +4,7 @@ export default function ProfileDefault({ children }) {
   return (
     <div>
       <h1>나의 포트폴리오</h1>
+      <h3>주제: 자전거 추천 사이트</h3>
 
       <div className="box1">
         <div>1학기</div>
@@ -17,10 +18,10 @@ export default function ProfileDefault({ children }) {
         <div>2학기</div>
         <hr />
         1-1: Nextshop 테스트 예제 주소(github) :
-        https://github.com/kmw1122/nextshop 1-2: Nextshop 테스트 예제
-        주소(vercel) : 2-1: 개인 포트폴리오 웹사이트(github) :
-        https://github.com/kmw1122/nextshop 2-2: 개인 포트폴리오
-        웹사이트(vercel) : https://nextshop-z4ws.vercel.app/
+        https://github.com/kmw1122/nextshop 1-2 : Nextshop 테스트 예제
+        주소(vercel) : (x) 2-1: 개인 포트폴리오 웹사이트(github) :
+        https://github.com/kmw1122/KMWwebpage 2-2 : 개인 포트폴리오
+        웹사이트(vercel) : https://kmwwebpage-kmw1122.vercel.app/
       </div>
     </div>
   )
